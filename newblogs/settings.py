a1798fa48b5ea42052454b8090b507f61fe9e1ac
Abdulahkenneh <abulayeekennedy@gmail.com>
2024-06-12 14:27:50 +0200

    common

--- newblogs/settings.py
+++ newblogs/settings.py
@@ -25,6 +25,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -110,6 +111,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 db_rom_env = dj_database_url.config('conn_max_age=600')
 DATABASES['default'].update(db_rom_env)
